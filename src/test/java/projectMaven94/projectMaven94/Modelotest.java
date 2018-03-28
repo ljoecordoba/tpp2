0ec8d69dde53c94cebed40cfd3cf675cd9e97eb8
Luciano Joel Cordoba <ljoecordoba@gmail.com>
2018-03-28 20:49:46 -0300

    Commiting once more time

--- src/test/java/projectMaven94/projectMaven94/Modelotest.java
+++ src/test/java/projectMaven94/projectMaven94/Modelotest.java
@@ -5,18 +5,7 @@ import modelo.Modelo;
 
 public class Modelotest {
 
-	@org.junit.Test
-	@org.junit.Before
-	public void correrAntes()
-	{
-		ControladorTest ctrl = new ControladorTest();
-		ctrl.Test2();
-		ctrl.Test2();
-		ctrl.Test3();
-		ctrl.Test4();
-		ctrl.test5();
-	}
-	
+
 	@org.junit.Test
 	public void test() {
 		Modelo modelo = new Modelo();
